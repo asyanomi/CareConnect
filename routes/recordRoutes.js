02581000eda95555eb85041ba5defc7bb8945807
asyanomi <tasyaanjelika53@gmail.com>
2026-07-01 12:54:42 +0700

    Move multer config

--- routes/recordRoutes.js
+++ routes/recordRoutes.js
@@ -1,6 +1,6 @@
 const express  = require('express');
 const router   = express.Router();
-const upload   = require('../uploads/mutler');
+const upload   = require('../config/mutler');
 const {
   createRecord,
   getMyRecords,
